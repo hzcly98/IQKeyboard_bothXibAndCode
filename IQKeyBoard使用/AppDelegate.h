dda15d10529d44816fbf126acc073a7af69ae940
黄章成 <huangzhangcheng@liminwang.local>
2017-07-27 15:35:49 +0800

    Initial Commit

---
+++ IQKeyBoard使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IQKeyBoard使用
+//
+//  Created by 黄章成 on 2017/7/27.
+//  Copyright © 2017年 黄章成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IQKeyBoard使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IQKeyBoard使用
+//
+//  Created by 黄章成 on 2017/7/27.
+//  Copyright © 2017年 黄章成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
